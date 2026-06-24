c4d22d2e217c909adf822dba42dfe4a566c9d462
monkeydluffyx183-beep <monkeydluffyx183@gmail.com>
2026-06-24 09:36:48 +0530

    Revert "Update from task b1bfbd37-62e5-412b-9690-b99b9df1d9dd"

--- rag-chatbot/backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.document import Document, DocumentType, DocumentStatus
-from app.models.conversation import Conversation, Message
-
-__all__ = ["User", "Document", "DocumentType", "DocumentStatus", "Conversation", "Message"]
